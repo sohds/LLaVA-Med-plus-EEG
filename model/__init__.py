e1111adfab3366703ee641c4808d9c8d1a2311fb
drawcodeboy <20rnjsekdns@naver.com>
2024-03-08 21:04:27 +0900

    LLaMA 관련 패키지 작업 중...

---
+++ model/__init__.py
@@ -0,0 +1,3 @@
+from . import llama
+from . import llava_med
+from . import transformers_not_downloaded
---
+++ model/llama/__init__.py
@@ -0,0 +1,3 @@
+# llama 패키지에서 LlamaConfig를 쓸 수 있도록 함.
+from .configuration_llama import LlamaConfig
+from .modeling_llama import LlamaModel
